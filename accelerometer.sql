b5b8f3729455853397c4b3690c3dd825b309aaa1
Attique Ahmed <74119758+AttiqueAhmed@users.noreply.github.com>
2024-03-31 04:33:02 +0600

    Update accelerometer.sql

--- accelerometer.sql
+++ accelerometer.sql
@@ -1,7 +1,8 @@
-CREATE TABLE accelerometer (
+CREATE EXTERNAL TABLE IF NOT EXISTS Accelerometer (
     user VARCHAR(255),
     timestamp BIGINT,
     x FLOAT,
     y FLOAT,
     z FLOAT
-);
+)
+LOCATION 's3://project3parent/accelerometer/landing/';
