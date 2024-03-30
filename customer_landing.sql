4e530b52e23d492dafb05256a834005f3b7eead9
Attique Ahmed <74119758+AttiqueAhmed@users.noreply.github.com>
2024-03-30 22:15:43 +0600

    Create customer_landing.sql

---
+++ customer_landing.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Customer (
+    customerName VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(20),
+    birthDay DATE,
+    serialNumber VARCHAR(50),
+    registrationDate BIGINT,
+    lastUpdateDate BIGINT,
+    shareWithResearchAsOfDate BIGINT,
+    shareWithPublicAsOfDate BIGINT,
+    shareWithFriendsAsOfDate BIGINT
+);
