c49b904e6a06177a30a308fb437e1e09409fdf59
Attique Ahmed <74119758+AttiqueAhmed@users.noreply.github.com>
2024-03-31 04:34:58 +0600

    Update step_trainer.sql

--- step_trainer.sql
+++ step_trainer.sql
@@ -1,5 +1,6 @@
-CREATE TABLE Step_trainer (
+CREATE EXTERNAL TABLE IF NOT EXISTS step_trainer (
     sensorReadingTime BIGINT,
     serialNumber VARCHAR(50),
     distanceFromObject INT
-);
+)
+LOCATION 's3://project3parent/step_trainer/landing/';
